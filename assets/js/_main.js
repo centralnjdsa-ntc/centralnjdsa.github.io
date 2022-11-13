631e96c73000cb7addd6887afca0c474a6e77913
mikestralka <masstralka@gmail.com>
2022-11-12 19:56:21 -0500

    Add notes about how to update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,6 +1,13 @@
 /* Custom UI settings and UI initialization.
  */
 
+/* This script is NOT referenced in _includes/scripts.html.
+ * You should make changes here, and then run the command
+ * npm run build:js 
+ * defined in package.json to minify this file to main.min.js.
+ * This requires installing node.js and the npm packages first.
+ */
+
 $(document).ready(function () {
     // Sticky footer, (Borrowed from minimal mistakes theme code)
     var bumpIt = function () {
